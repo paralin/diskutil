100f350ed28ca49034784b0f09650437f9532543
Christian Stewart <christian@paral.in>
2018-10-08 16:17:03 -0700

    feat: unsupported error at runtime
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ diskutil_unsupported.go
@@ -0,0 +1,25 @@
+//+build !windows
+
+package diskutil
+
+import (
+	"runtime"
+
+	"github.com/pkg/errors"
+)
+
+var errUnsupported = errors.Errorf("unsupported on platform: %s @ %s", runtime.GOOS, runtime.GOARCH)
+
+// FlashToDisk clears a disk and flashes a image.
+// Progress callback is called with updates.
+func FlashToDisk(
+	diskPath string,
+	progressCb func(percent int, status string),
+) error {
+	return errUnsupported
+}
+
+// ListStorageDevices lists all connected storage devices.
+func ListStorageDevices() ([]*DeviceDescriptor, error) {
+	return nil, errUnsupported
+}
--- diskutil_windows.go
+++ diskutil_windows.go
@@ -1,8 +1,12 @@
 package diskutil
 
 import (
+	"io/ioutil"
+	"os"
+	"os/exec"
 	"regexp"
 	"strconv"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -25,7 +29,46 @@ func FlashToDisk(
 	//  - select disk 3
 	//  - clean
 	//  - rescan
-	_ = diskNum
+	scrFile, err := ioutil.TempFile("", "*.txt")
+	if err != nil {
+		return errors.Wrap(err, "open temporary file")
+	}
+	scrFilePath := scrFile.Name()
+	// defer os.Remove(scrFilePath)
+
+	progressCb(5, "Cleaning disk (may request elevation)...")
+	scrFile.WriteString("select disk ")
+	scrFile.WriteString(strconv.Itoa(diskNum))
+	scrFile.WriteString("\nclean\nrescan\n")
+	if err := scrFile.Close(); err != nil {
+		return errors.Wrap(err, "close temporary file")
+	}
+
+	// requires elevation
+	// diskPart := exec.Command("diskpart", "/s", scrFilePath)
+	// powershell -ExecutionPolicy ByPass -Command "Start-Process cmd -ArgumentList '/c','diskpart','/s','C:\Users\kidov\Desktop\diskutil-script.txt'" -Verb RunAs
+	diskPart := exec.Command(
+		"powershell",
+		"-ExecutionPolicy", "ByPass",
+		"-Command", "Start-Process cmd -ArgumentList '/c','diskpart','/s','"+scrFilePath+"'",
+		"-Verb", "RunAs",
+	)
+	if err := diskPart.Run(); err != nil {
+		return errors.Wrap(err, "diskpart clean disk")
+	}
+
+	// wait 2 seconds for partitions to update
+	for i := 0; i < 10; i++ {
+		progressCb(10+i, "Waiting for Windows to rescan the disk...")
+		<-time.After(time.Millisecond * 200)
+	}
+
+	progressCb(23, "Opening disk...")
+	f, err := os.OpenFile(diskPath, os.O_SYNC|os.O_RDWR, 0777)
+	if err != nil {
+		return err
+	}
+	f.Close()
 
 	return nil
 }
